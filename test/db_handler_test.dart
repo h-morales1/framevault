0a071a447855ece4939dbc81f6600613a9092bc3
Herbert M. <hmorales5@tutanota.com>
2024-01-14 08:12:00 -0600

    added sqflite and test packages

---
+++ test/db_handler_test.dart
@@ -0,0 +1,10 @@
+import 'package:frame_vault/db/db_handler.dart';
+import 'package:test/test.dart';
+
+@Skip('Not fully implemented')
+void main() {
+  test('table existence', () {
+    final dbhandler = DbHandler();
+    // write test
+  });
+}
